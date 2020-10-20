5addd71cab663e7e6aadfb24d95655a943c40c28
rtwele <ryantwele@gmail.com>
2020-10-19 20:30:03 -0400

    homework

---
+++ app.js
@@ -0,0 +1,35 @@
+var cat = {
+    name: 'Garfield',
+    legs: 4,
+    enemies: ['water', 'dogs']
+};
+
+var dog = {
+    name: 'Baxter',
+    legs: 4,
+    enemies: ['cats', 'mailman']
+};
+
+var bird = {
+    name: 'Kiwi',
+    legs: 2,
+    enemies: ['mice', 'windows' ]
+};
+
+console.log(cat.legs);
+console.log(dog.name);
+console.log(bird.enemies);
+
+function myFunction(arr) {
+    console.log(myFunction(arr));
+}
+
+var arr = [1,2,3,4,5,6,7,8];
+for (let i = 0; i < arr.length; i++) {
+  console.log(arr[i]);
+}
+
+
+function myFunction(arr) {
+    console.log(myFunction(arr));
+}
